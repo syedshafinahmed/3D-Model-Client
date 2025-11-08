b1a2e1292060d8341ba177e834efa3c975c83b9a
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-11-08 14:31:01 +0600

    Implemented Case-Insensitive Search

--- src/Pages/AllModels/AllModels.jsx
+++ src/Pages/AllModels/AllModels.jsx
@@ -1,9 +1,12 @@
 import { useLoaderData } from "react-router";
 import { ModelCard } from "../../components/ModelCard";
 import { useState } from "react";
+import { BeatLoader } from "react-spinners";
 
 const AllModels = () => {
   const data = useLoaderData();
+  const [models, setModels] = useState(data);
+  const [loading, setLoading] = useState(false);
   // const [models, setModels] = useState(data)
   // const [loading, setLoading] = useState(false)
 
@@ -22,8 +25,27 @@ const AllModels = () => {
   //     })
   // }
 
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const search = e.target.search.value;
+    setLoading(true);
+    fetch(`http://localhost:3000/search?search=${search}`)
+      .then(res => res.json())
+      .then(data => {
+        // console.log(data)
+        setModels(data);
+        setLoading(false);
+      })
+  }
 
 
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <BeatLoader color="#db2777" />
+      </div>
+    );
+  }
 
 
 
@@ -55,13 +77,19 @@ const AllModels = () => {
         <button className="btn btn-secondary  rounded-full">{loading ? "Searching...." : "Search"}</button>
       </form> */}
 
-      <div className="grid grid-cols-3 lg:grid-cols-4 gap-3">
-        {/* {models.map((model) => (
-          <ModelCard key={model._id} model={model} />
-        ))} */}
-       {
-        data.map(model => <ModelCard model={model} key={model._id}></ModelCard>)
-       }
+      <div className="flex justify-center items-center pt-10 pb-20 px-10">
+        <form onSubmit={handleSearch}>
+          <div className="w-full max-w-lg mx-auto relative">
+            <input name="search" type="text" placeholder="Search..." className="w-full rounded-full border border-gray-300 px-4 py-2 pr-24 focus:outline-none" />
+            <button className="absolute right-1 top-1/2 -translate-y-1/2 rounded-full bg-linear-to-r from-pink-600 to-red-500 text-white px-4 py-1 hover:from-pink-700 hover:to-red-600">Search</button>
+          </div>
+        </form>
+      </div>
+
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+        {
+          models.map(model => <ModelCard model={model} key={model._id}></ModelCard>)
+        }
       </div>
     </div>
   );
